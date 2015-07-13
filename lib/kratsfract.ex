129197083ed47484e7fff1872ac3b8000e392008
Martin Frost <frost@ceri.se>
2015-07-13 19:31:31 +0200

    Only run as many tasks as there are schedulers

--- lib/kratsfract.ex
+++ lib/kratsfract.ex
@@ -1,5 +1,4 @@
 defmodule KratsFract do
-
   def main(argv) do
     {options, [], []} = OptionParser.parse(argv,
               strict: [width: :integer, height: :integer, scale: :float,
@@ -18,35 +17,34 @@ defmodule KratsFract do
     w_2 = div width, 2
     h_2 = div height, 2
     s = scale / min(w_2, h_2)
+    schedulers = :erlang.system_info(:schedulers_online)
+    lines_per_scheduler = div(height, schedulers)
 
     file = File.open! "foo.pgm", [:write]
     IO.binwrite file, "P5\n"
     IO.binwrite file, "#{width} #{height}\n"
     IO.binwrite file, "#{maxiter}\n"
-    for row <- 0 .. height - 1, column <- 0 .. width - 1 do
-      {column, row}
+
+    for i <- 1 .. schedulers do
+      Task.async(fn ->
+        low = lines_per_scheduler * (i - 1)
+        high = low + lines_per_scheduler - 1
+        for row <- low .. high, column <- 0 .. width - 1 do
+          apply(__MODULE__,
+                compute_function,
+                [{s * (column - w_2), s * (row - h_2)}, maxiter])
+        end
+      end)
     end
-    |> Stream.chunk(4000)
-    |> Enum.map(fn chunk ->
-      Task.async(__MODULE__, compute_function, [w_2, h_2, s, chunk, maxiter])
-    end)
     |> Enum.map(&Task.await/1)
     |> (fn data -> IO.binwrite(file, data) end).()
   end
 
-  def compute_mandelbrot(w_2, h_2, s, chunk, maxiter) do
-    for {column, row} <- chunk, do:
-      Fractal.julia({0.0, 0.0},
-                    {s * (column - w_2), s * (row - h_2)},
-                    0,
-                    maxiter)
+  def compute_mandelbrot(n, maxiter) do
+    Fractal.julia({0.0, 0.0}, n, 0, maxiter)
   end
 
-  def compute_julia(w_2, h_2, s, chunk, maxiter) do
-    for {column, row} <- chunk, do:
-      Fractal.julia({s * (column - w_2), s * (row - h_2)},
-                    {-0.75, 0.14},
-                    0,
-                    maxiter)
+  def compute_julia(n, maxiter) do
+    Fractal.julia(n, {-0.75, 0.14}, 0, maxiter)
   end
 end
